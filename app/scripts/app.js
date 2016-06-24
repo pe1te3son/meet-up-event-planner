50dcf5c413b4c4cff7375f48e2d7834b1261ac69
Peter Janak <2010janak2010@gmail.com>
2016-06-24 02:16:49 +0100

    Initial commit
    This is very first commit. I have installed angular framework with
    yeaoman and set linter, gitignore and sass. Bootstrap doesn`t support
    latest version of jquery so i set to use jquery 2.1.3 instead in
    bower.json

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name eventPlannerApp
+ * @description
+ * # eventPlannerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('eventPlannerApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
