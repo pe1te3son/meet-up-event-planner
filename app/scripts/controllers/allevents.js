551dbc5713c5d089a67b27c742d00ffeaac7ec4c
Peter Janak <2010janak2010@gmail.com>
2016-06-27 00:18:06 +0100

    Added controller for all events to display

---
+++ app/scripts/controllers/allevents.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eventPlannerApp.controller:AlleventsCtrl
+ * @description
+ * # AlleventsCtrl
+ * Controller of the eventPlannerApp
+ */
+angular.module('eventPlannerApp')
+  .controller('AlleventsCtrl', ['$firebaseArray', 'firebaseHelper', function ($firebaseArray, firebaseHelper) {
+
+    var ref = new Firebase( firebaseHelper.link() + '/events');
+
+    this.allEvents = $firebaseArray(ref);
+    
+  }// function ends
+
+]);
---
+++ test/spec/controllers/allevents.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Controller: AlleventsCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('eventPlannerApp'));
+
+  var AlleventsCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    AlleventsCtrl = $controller('AlleventsCtrl', {
+      $scope: scope
+      // place here mocked dependencies
+    });
+  }));
+
+  it('allEvents should be an array', function () {
+    expect(AlleventsCtrl.allEvents).toEqual(jasmine.any(Array));
+  });
+});
