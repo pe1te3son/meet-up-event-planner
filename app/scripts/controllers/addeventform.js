f35cb28ff2b24f2ea927915b7264f23d7a18e0e8
Peter Janak <2010janak2010@gmail.com>
2016-06-27 04:52:23 +0100

    Added extra input fields
    Completed all required fields for "add event form". Date inputs, host
    and guests. Date inputs are being set with moment.js

--- app/scripts/controllers/addeventform.js
+++ app/scripts/controllers/addeventform.js
@@ -9,24 +9,51 @@
  */
 angular.module('eventPlannerApp')
   .controller('addEventFormCtrl', ['$firebaseArray', 'firebaseHelper', function ($firebaseArray, firebaseHelper) {
-    var ref = new Firebase(firebaseHelper.link() + '/events');
 
-    this.firebaseData = $firebaseArray(ref);
-    this.event = {
-      name: '',
-      category: '',
-      host: '',
-      additionalInfo: ''
+    var ref = new Firebase(firebaseHelper.link() + '/events');
+    var vm = this;
+    var date = moment().format('YYYY-MM-DD');
+    var time = moment().format('hh:mm');
+
+    this.setDate = date + 'T' + time;
+    this.collection = $firebaseArray(ref);
+
+    this.eventDefault = function(){
+      this.selectedStartDate = new Date(this.setDate);
+      this.selectedEndDate = this.selectedStartDate;
+      return {
+        name: '',
+        category: '',
+        startDate: date,
+        startTime: time,
+        endDate: '',
+        endTime: '',
+        host: '',
+        guests: ['first', 'second'],
+        eventLocation: '',
+        additionalInfo: ''
+      };
     };
 
+    this.event = this.eventDefault();
+
     this.addEvent = function(){
-      this.firebaseData.$add(this.event);
-      this.event = {};
+      vm.event.startDate = moment(vm.selectedStartDate).format('YYYY-MM-DD');
+      vm.event.startTime = moment(vm.selectedStartDate).format('hh:mm');
+      vm.event.endDate = moment(vm.selectedEndDate).format('YYYY-MM-DD');
+      vm.event.endTime = moment(vm.selectedEndDate).format('hh:mm');
+      vm.collection.$add(vm.event);
+      vm.event = vm.eventDefault();
     };
 
     this.submit = function(){
-      this.addEvent();
+      vm.addEvent();
     };
+
+    this.updateEndDate = function(){
+      this.selectedEndDate = this.selectedStartDate;
+    };
+
   }
 
 ]);
