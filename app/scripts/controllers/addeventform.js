4e8302e6fc9016369f4dff46690603a1d71027f2
Peter <2010janak2010@gmail.com>
2016-07-14 03:54:38 +0100

    View reloads when add event form submited, comments

--- app/scripts/controllers/addeventform.js
+++ app/scripts/controllers/addeventform.js
@@ -9,17 +9,28 @@
  */
 
 angular.module('eventPlannerApp')
-  .controller('AddeventformCtrl', ['$firebaseArray', 'FireBase', function ($firebaseArray, FireBase) {
+  .controller('AddeventformCtrl', ['$firebaseArray', 'FireBase', '$state', function ($firebaseArray, FireBase, $state) {
 
     var ref = new Firebase(FireBase.link + '/events');
     var vm = this;
     var date = moment().format('YYYY-MM-DD');
     var time = moment().format('hh:mm');
+
+    // Formated date for html input field with attribute type=datetime-local
     this.setDate = date + 'T' + time;
     this.collection = $firebaseArray(ref);
+
+    // Updated end event time and date base on start date and time selected
+    this.updateEndDate = function(){
+      vm.selectedEndDate = vm.selectedStartDate;
+    };
+
+    // Returns all default values for add event form
     this.eventDefault = function(){
       vm.selectedStartDate = new Date(vm.setDate);
-      vm.selectedEndDate = vm.selectedStartDate;
+      vm.updateEndDate();
+
+      // All values commented out are optional
       return {
         name: '',
         category: '',
@@ -30,40 +41,40 @@ angular.module('eventPlannerApp')
         host: '',
         guests: [],
         eventLocation: '',
-        additionalInfo: ''
+        //additionalInfo: ''
       };
     };
 
+    // Set defaults
     this.event = this.eventDefault();
 
     this.addEvent = function(){
+
+      // Format time and date before saving to database
       vm.event.startDate = moment(vm.selectedStartDate).format('YYYY-MM-DD');
       vm.event.startTime = moment(vm.selectedStartDate).format('hh:mm');
       vm.event.endDate = moment(vm.selectedEndDate).format('YYYY-MM-DD');
       vm.event.endTime = moment(vm.selectedEndDate).format('hh:mm');
+
+      // Save to database
       vm.collection.$add(vm.event);
-      vm.event = vm.eventDefault();
     };
 
     this.submit = function(isValid){
       if(isValid){
         vm.addEvent();
+
+        // Reload add form to clear inputs
+        $state.reload();
       }
     };
 
-    this.updateEndDate = function(){
-      vm.selectedEndDate = vm.selectedStartDate;
-    };
-
-
-
     this.addGuest = function(e){
       var duplicate = false;
-      console.log(e);
+
       for(var i = 0; i<vm.event.guests.length && !duplicate; i++){
         if(vm.guest === vm.event.guests[i]){
           duplicate = true;
-          console.log(duplicate);
         }
       }
 
@@ -71,16 +82,13 @@ angular.module('eventPlannerApp')
 
         if(!duplicate){
           vm.event.guests.push(vm.guest);
-          console.log(vm.guest.length);
           vm.guest = ' ';
-
         }
 
       }
     };
 
     this.removeGuest = function(index){
-      console.log(index);
       vm.event.guests.splice(index, 1);
     };
 
