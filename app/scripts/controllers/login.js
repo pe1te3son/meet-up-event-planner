6d446bc63494f32077e5554cf9e276b2abc2e209
Peter Janak <2010janak2010@gmail.com>
2016-07-15 04:14:42 +0100

    Added register user function

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,16 +8,18 @@
  * Controller of the eventPlannerApp
  */
 angular.module('eventPlannerApp')
-  .controller('LoginCtrl', ['$firebaseAuth', 'FirebaseService', '$state', '$stateParams', '$rootScope', function ($firebaseAuth, FirebaseService, $state, $stateParams, $rootScope) {
-    var ref = new Firebase(FirebaseService.link);
-    var auth = $firebaseAuth(ref);
+  .controller('LoginCtrl', ['FirebaseService', '$state', '$stateParams', '$rootScope', function (FirebaseService, $state, $stateParams, $rootScope) {
     var vm = this;
 
     this.sc = $rootScope;
 
     this.$state = $state.current.name;
-    this.invalidPassword = false;
     this.rememberMe = true;
+    this.spinner = false;
+    this.invalidPassword = false;
+    this.invalidUser = false;
+    this.invalidPassword = false;
+    this.logInError = false;
 
     this.details = {
       /**
@@ -46,38 +48,50 @@ angular.module('eventPlannerApp')
     };
 
     this.loginUser = function(isValid){
-      //console.log(vm);
+
       if(isValid){
         vm.spinner = true;
-        auth.$authWithPassword({
+        FirebaseService.auth().$authWithPassword({
           email: vm.details.email,
           password: vm.details.password
         }, { remember: vm.rememberMeFunc(vm.rememberMe) }).then(function(authData) {
-
-          console.log('Logged in as:', authData.uid);
+          console.log(authData);
           vm.userLoggedIn('peter');
           vm.spinner = false;
 
         }).catch(function(error) {
           switch (error.code) {
             case 'INVALID_PASSWORD':
-                //errorMsg += '* The specified user account or password is incorrect.';
                 console.log('The specified user account or password is incorrect.');
                 vm.invalidPassword = true;
                 vm.details.password = '';
                 break;
             case 'INVALID_USER':
-                //errorMsg += '* The specified user account does not exist.';
                 console.log('The specified user account does not exist.');
                 vm.invalidUser = true;
                 break;
             default:
                 vm.logInError = true;
-                //errorMsg = 'Error logging user in: ' + error;
           }
-
           vm.spinner = false;
+        });
+      }
 
+    };
+
+    this.registerUser = function(isValid){
+
+      if(isValid){
+        vm.spinner = true;
+        FirebaseService.auth().$createUser({
+          email: vm.details.email,
+          password: vm.details.password
+        }).then(function(userData) {
+          console.log("User created with uid: " + userData.uid);
+          $state.go('login');
+
+        }).catch(function(error) {
+          console.log(error);
         });
       }
 
