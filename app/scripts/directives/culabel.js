ed1cdb637738d0ab1e97682d4f0b10dce5805f8c
Peter Janak <2010janak2010@gmail.com>
2016-07-16 00:48:53 +0100

    Added onchange for inputs to fire label animation on  autocomplete

--- app/scripts/directives/culabel.js
+++ app/scripts/directives/culabel.js
@@ -13,27 +13,28 @@ angular.module('eventPlannerApp')
       //scope, element, attrs
       link: function(scope, element) {
 
+        element.change(function(){
+          var el = element.parent().find('label');
+          if(element.val().length){
+            el.addClass('label-animated');
+          }
+        });
+
         element.on('focus', function(){
           var el = element.parent().find('label');
-
           if(el.hasClass('label-animated-off')){
             el.removeClass('label-animated-off');
           }
-
           el.addClass('label-animated highlight');
-
         });
 
         element.on('blur', function(){
           var el = element.parent().find('label');
-
           if(!element.val().length){
             el.removeClass('label-animated');
             el.addClass('label-animated-off');
           }
-
           el.removeClass('highlight');
-
         });
 
       }
