1429f6c0c2a47906154c92cd1fa87e411ca47e94
Peter Janak <2010janak2010@gmail.com>
2016-07-21 04:27:55 +0100

    Added function that displays all information when event clicked

--- app/scripts/controllers/allevents.js
+++ app/scripts/controllers/allevents.js
@@ -11,6 +11,22 @@ angular.module('eventPlannerApp')
   .controller('AlleventsCtrl', ['FirebaseService', function (FirebaseService) {
 
     this.allEvents = FirebaseService.array('/events');
+    this.remove = function(item){
+      console.log(item);
+    };
+
+
+   this.select= function(item) {
+    console.log('select');
+    this.selected = item;
+
+   };
+
+   this.isActive = function(item) {
+     console.log('class');
+    return this.selected === item;
+   };
+
 
   }// function ends
 
