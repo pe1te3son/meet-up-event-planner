a7377d2a7287470f011b69848f67441a5b4d5053
Peter Janak <2010janak2010@gmail.com>
2016-08-05 01:04:19 +0100

    docs: Description update

--- app/scripts/controllers/eventmodal.js
+++ app/scripts/controllers/eventmodal.js
@@ -4,15 +4,18 @@
  * @description
  * # EventmodalCtrl
  * Controller of the eventPlannerApp
+ * Shows and hides modal window and deletes selected event.
+ * When modal close it redirects to allEvents
  */
 angular.module('eventPlannerApp')
   .controller('EventmodalCtrl', [ 'FirebaseService', 'eventdata', '$state',  function (FirebaseService, eventdata, $state) {
     'use strict';
-    
+
     var vm = this;
     this.eventdata = eventdata;
 
     $('#event-modal').modal('show').on('hide.bs.modal', function(){
+      // Waits for animation to end and then redirects
       setTimeout(function () {
         $state.go('allEvents');
       }, 200);
@@ -22,6 +25,7 @@ angular.module('eventPlannerApp')
       return moment(date, 'YYYY-MM-DD').format('Do MMM YYYY');
     };
 
+    // Removes event from database
     this.remove = function(){
       var data = FirebaseService.array('/events');
       data.$loaded()
